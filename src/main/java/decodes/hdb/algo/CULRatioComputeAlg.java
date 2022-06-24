8c5b88865612f735f93ca19a4e57d710881010b2
Andrew Gilmore <agilmore2@gmail.com>
2022-06-23 23:02:48 -0600

    Initial CUL ratio computation algorithm and cleanup
    Signed-off-by: Andrew Gilmore <agilmore2@gmail.com>

--- src/main/java/decodes/hdb/algo/CULRatioComputeAlg.java
+++ src/main/java/decodes/hdb/algo/CULRatioComputeAlg.java
@@ -6,12 +6,10 @@ import decodes.hdb.dbutils.RBASEUtils;
 import decodes.tsdb.DbCompException;
 import decodes.tsdb.ParmRef;
 import decodes.tsdb.algo.AWAlgoType;
-import decodes.util.DecodesSettings;
 import decodes.util.PropertySpec;
 import ilex.var.NamedVariable;
 
 import java.sql.Connection;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -22,55 +20,64 @@ import java.util.regex.Pattern;
 //AW:JAVADOC
 
 /**
- This algorithm computes the weighted monthly coefficients from source data
- for use in disaggregation from annual to monthly values for other years
- Only applies to R_
+ This algorithm computes the average ratio between two timeseries for use in estimating the
+ distribution of the timeseries when only the total is known.
+ Only applies to R_MONTH
 
  ESTIMATE_PROCESS: Algorithms used on the input data that are NOT considered source data
- NO_ROUNDING: determines if rounding to the 5th decimal point is desired, default FALSE
+ ROUNDING: determines if rounding to the 7th decimal point is desired, default FALSE
 
  Algorithm is as follows:
- query average of all input data grouped by month
+ query average ratio of one timeseries to the sum of the two grouped by month
  store 12 values in r_month table in a specific year, 1985 by convention, set by computation output variables
  decide if want to check for update or just have database catch duplicates
 
- working SQL query to compute data:
- WITH t AS
- (SELECT vals.VALUE, EXTRACT(MONTH FROM vals.start_date_time) mon, EXTRACT(YEAR FROM vals.start_date_time) yr
- FROM hdb_site_datatype sd, r_month vals, r_base base, hdb_loading_application app
- WHERE
- site_id = 4856 AND datatype_id = 1374 AND
- sd.site_datatype_id = vals.site_datatype_id and
- sd.site_datatype_id = base.site_datatype_id AND
- base.INTERVAL = 'month' AND base.start_date_time = vals.start_date_time AND
- base.loading_application_id = app.loading_application_id and
- app.loading_application_name != 'CU_estimation_process' -- string from property
- ),
- yrs AS (SELECT yr, sum(VALUE) tot, count(VALUE) mons FROM t GROUP BY yr)
- SELECT mon, round(avg(VALUE/tot),4)*100 coef FROM t, yrs
- WHERE t.yr = yrs.yr AND
- yrs.mons = 12 -- comment out if ignore_partials false
- group by mon order by mon;
+ working? SQL query to compute data:
+  WITH t AS
+ (SELECT extract(year from a.start_date_time) yr, A.VALUE/(A.VALUE+b.VALUE) ratio
+ FROM
+ r_year A, r_year b, r_base ab, r_base bb WHERE
+ A.site_datatype_id = 25059 AND -- pull from input1
+ b.site_datatype_id = 25058 AND -- pull from input2
+ A.start_date_time = b.start_date_time AND
+ -- join with r_base for input1 to check loading app
+ A.site_datatype_id = ab.site_datatype_id AND
+ ab.INTERVAL = 'year' AND
+ ab.start_date_time = A.start_date_time AND
+ ab.loading_application_id NOT IN
+  (SELECT loading_application_id FROM
+  hdb_loading_application WHERE loading_application_name IN ('CU_estimation_process')
+  ) AND
+ -- same for input2
+ b.site_datatype_id = bb.site_datatype_id AND
+ bb.INTERVAL = 'year' AND
+ bb.start_date_time = b.start_date_time AND
+ bb.loading_application_id NOT IN
+  (SELECT loading_application_id FROM
+  hdb_loading_application WHERE loading_application_name IN ('CU_estimation_process')
+  )
+ )
+ SELECT  avg(ratio) FROM t
+ ;
 
- querying r_month for actual values to ensure validation has occurred.
+ querying r_year for actual values to ensure validation has occurred.
  querying r_base to get loading application info
- will use ignore_partials by default
 
  */
 //AW:JAVADOC_END
-public class CULSourceDistributionComputeAlg
+public class CULRatioComputeAlg
         extends decodes.tsdb.algo.AW_AlgorithmBase
 {
     //AW:INPUTS
-    public double input;	//AW:TYPECODE=i
-    String[] _inputNames = { "input" };
+    public double input1;	//AW:TYPECODE=i
+    public double input2;	//AW:TYPECODE=i
+    String[] _inputNames = { "input1", "input2" };
 //AW:INPUTS_END
 
     //AW:LOCALVARS
     // Enter any local class variables needed by the algorithm.
     String alg_ver = "1.0";
     String query;
-    int count = 0;
     boolean do_setoutput = true;
     Connection conn = null;
 
@@ -78,12 +85,12 @@ public class CULSourceDistributionComputeAlg
 
     PropertySpec[] specs =
             {
-                    new PropertySpec("ignore_partials", PropertySpec.BOOLEAN,
-                            "(default=true) If true, then only compute coefficients for years with all 12 months."),
                     new PropertySpec("rounding", PropertySpec.BOOLEAN,
                             "(default=false) If true, then rounding is done on the output value."),
                     new PropertySpec("validation_flag", PropertySpec.STRING,
                             "(empty) Always set this validation flag in the output."),
+                    new PropertySpec("flags", PropertySpec.STRING,
+                            "(empty) Always set these dataflags in the output."),
                     new PropertySpec("estimation_process", PropertySpec.STRING,
                             "(CU_estimation_process) Which loading application produces estimates that should be ignored."),
                     new PropertySpec("coeff_year", PropertySpec.INT,
@@ -100,15 +107,13 @@ public class CULSourceDistributionComputeAlg
 //AW:OUTPUTS_END
 
     //AW:PROPERTIES
-    public boolean ignore_partials = true;
     public boolean rounding = false;
     public String estimation_process = "CU_estimation_process";
     public String validation_flag = "";
     public Integer coeff_year = 1985;
     public String flags;
 
-    String[] _propertyNames = { "ignore_partials", "estimation_process",
-            "validation_flag", "rounding", "coeff_year" };
+    String[] _propertyNames = { "estimation_process", "validation_flag", "rounding", "coeff_year", "flags" };
 //AW:PROPERTIES_END
 
     // Allow javac to generate a no-args constructor.
@@ -146,9 +151,7 @@ public class CULSourceDistributionComputeAlg
         }
 
         query = null;
-        count = 0;
         do_setoutput = true;
-        flags = "";
         conn = null;
 //AW:BEFORE_TIMESLICES_END
     }
@@ -188,22 +191,34 @@ public class CULSourceDistributionComputeAlg
         do_setoutput = true;
 
         // get the input and output parameters and see if its model data
-        ParmRef parmRef = getParmRef("input");
-        if (parmRef == null) {
-            warning("Unknown variable 'INPUT'");
+        ParmRef inRef1 = getParmRef("input1");
+        if (inRef1 == null) {
+            warning("Unknown variable 'INPUT1'");
+            return;
+        }
+        ParmRef inRef2 = getParmRef("input2");
+        if (inRef2 == null) {
+            warning("Unknown variable 'INPUT2'");
             return;
         }
 
-        String input_interval = parmRef.compParm.getInterval();
-        if (input_interval == null || !input_interval.equals("month"))
-            warning("Wrong input interval for " + comp.getAlgorithmName());
-
-        String table_selector = parmRef.compParm.getTableSelector();
-        if (table_selector == null || !table_selector.equals("R_"))
+        String input_interval1 = inRef1.compParm.getInterval();
+        if (input_interval1 == null || !input_interval1.equals("year"))
+            warning("Wrong input1 interval for " + comp.getAlgorithmName());
+        String table_selector1 = inRef1.compParm.getTableSelector();
+        if (table_selector1 == null || !table_selector1.equals("R_"))
             warning("Invalid table selector for algorithm, only R_ supported");
 
-        parmRef = getParmRef("output1");
-        if (parmRef == null)
+        String input_interval2 = inRef2.compParm.getInterval();
+        if (input_interval2 == null || !input_interval2.equals("year"))
+            warning("Wrong input2 interval for " + comp.getAlgorithmName());
+        String table_selector2 = inRef2.compParm.getTableSelector();
+        if (table_selector2 == null || !table_selector2.equals("R_"))
+            warning("Invalid table selector for algorithm, only R_ supported");
+
+
+        ParmRef outRef = getParmRef("output1");
+        if (outRef == null)
             warning("Unknown output variable 'OUTPUT'");
 
         TimeZone tz = TimeZone.getTimeZone("GMT");
@@ -220,36 +235,38 @@ public class CULSourceDistributionComputeAlg
         dbobj.put("ALG_VERSION",alg_ver);
         conn = tsdb.getConnection();
         DBAccess db = new DBAccess(conn);
-        RBASEUtils rbu = new RBASEUtils(dbobj,conn);
 
-        String require_12_clause = "";
-        if (ignore_partials)
-        {
-            require_12_clause = " yrs.mons = 12 ";
-        }
-        String select_clause = " SELECT mon, avg(VALUE/tot) coef FROM t, yrs";
+        String select_clause = " SELECT avg(ratio) ratio FROM t";
         if (rounding)
         {
-            select_clause = " SELECT mon, round(avg(VALUE/tot),7) coef FROM t, yrs"; // 7 used by other HDB aggregates
+            select_clause = " SELECT round(avg(ratio),7) ratio FROM t"; // 7 used by other HDB aggregates
         }
 
-        query = " WITH t AS" +
-                " (SELECT vals.VALUE, EXTRACT(MONTH FROM vals.start_date_time) mon, EXTRACT(YEAR FROM vals.start_date_time) yr" +
-                " FROM hdb_site_datatype sd, r_month vals, r_base base, hdb_loading_application app" +
-                " WHERE" +
-                " sd.site_datatype_id = "+getSDI("input")+" AND" +
-                " sd.site_datatype_id = vals.site_datatype_id AND" +
-                " sd.site_datatype_id = base.site_datatype_id AND" +
-                " base.INTERVAL = 'month' AND base.start_date_time = vals.start_date_time AND" +
-                " base.loading_application_id = app.loading_application_id AND" +
-                " app.loading_application_name != '" + estimation_process + "' AND" +
-                " app.loading_application_name != 'compedit'" +
-                " )," +
-                " yrs AS (SELECT yr, sum(VALUE) tot, count(VALUE) mons FROM t GROUP BY yr)" +
-                select_clause + // SELECT mon, avg(VALUE/tot) coef FROM t, yrs -- or rounding version
-                " WHERE t.yr = yrs.yr AND" +
-                require_12_clause + // yrs.mons = 12 --adds requirement that only whole years are used to compute coefficients
-                " group by mon order by mon";
+        query = " WITH t AS\n" +
+                " (SELECT extract(year from a.start_date_time) yr, A.VALUE/(A.VALUE+b.VALUE) ratio\n" +
+                " FROM\n" +
+                " r_year A, r_year b, r_base ab, r_base bb WHERE\n" +
+                " A.site_datatype_id = 25059 AND -- pull from input1\n" +
+                " b.site_datatype_id = 25058 AND -- pull from input2\n" +
+                " A.start_date_time = b.start_date_time AND\n" +
+                " -- join with r_base for input1 to check loading app\n" +
+                " A.site_datatype_id = ab.site_datatype_id AND\n" +
+                " ab.INTERVAL = 'year' AND\n" +
+                " ab.start_date_time = A.start_date_time AND\n" +
+                " ab.loading_application_id NOT IN\n" +
+                "  (SELECT loading_application_id FROM\n" +
+                "  hdb_loading_application WHERE loading_application_name IN ('CU_estimation_process')\n" +
+                "  ) AND\n" +
+                " -- same for input2\n" +
+                " b.site_datatype_id = bb.site_datatype_id AND\n" +
+                " bb.INTERVAL = 'year' AND\n" +
+                " bb.start_date_time = b.start_date_time AND\n" +
+                " bb.loading_application_id NOT IN\n" +
+                "  (SELECT loading_application_id FROM\n" +
+                "  hdb_loading_application WHERE loading_application_name IN ('CU_estimation_process')\n" +
+                "  )\n" +
+                " )\n" +
+                select_clause; // SELECT avg(ratio) ratio FROM t -- or rounding version
 
 
         status = db.performQuery(query,dbobj); // interface has no methods for parameters
@@ -257,55 +274,37 @@ public class CULSourceDistributionComputeAlg
         debug3(" SQL STRING:" + query + "   DBOBJ: " + dbobj.toString() + "STATUS:  " + status);
         // now see if the aggregate query worked if not abort!!!
 
-        if (status.startsWith("ERROR"))
+        if (status.startsWith("ERROR") || (Integer.parseInt((String) dbobj.get("rowCount")) != 1)
+)
         {
-            warning(comp.getAlgorithmName()+"-"+alg_ver+" Aborted: see following error message");
+            warning(comp.getName()+"-"+alg_ver+" Aborted: see following error message");
             warning(status);
             return;
         }
-        // now retrieve records from coeff computation
-        ArrayList<Object> mons  = (ArrayList<Object>) dbobj.get("mon");
-        ArrayList<Object> coeffs = (ArrayList<Object>) dbobj.get("coef");
-
-        //  delete any existing resultant value if not enough values returned
-        if (mons.size() != 12)
-        {
-            debug3(comp.getAlgorithmName() + "-" + alg_ver + " Aborted: not enough month values " + _aggregatePeriodBegin + " SDI: " + getSDI("input"));
-            do_setoutput = false; // something went wrong, delete outputs
-        }
-
-//              otherwise we have some records so continue...
+        // now retrieve ratio
+        double ratio = Double.parseDouble((String) dbobj.get("ratio"));
 
         // set the output if all is successful and set the flags appropriately
+        cal.set(coeff_year, 0, 1, 0, 0); // Months are 0 indexed in Java dates
         if (do_setoutput) {
-            Iterator<Object> it1 = mons.iterator();
-            Iterator<Object> it2 = coeffs.iterator();
-            while (it1.hasNext() && it2.hasNext()) {
-                int mon = Integer.parseInt(it1.next().toString());
-                double coeff = Double.parseDouble(it2.next().toString());
-                cal.set(coeff_year, mon - 1, 1, 0, 0); // Months are 0 indexed in Java dates
 
-                debug3("FLAGS: " + flags);
-                if (flags != null)
-                    setHdbDerivationFlag(output, flags);
-                //
-                /* added to allow users to automatically set the Validation column */
-                if (validation_flag.length() > 0)
-                    setHdbValidationFlag(output, validation_flag.charAt(1));
+            debug3("FLAGS: " + flags);
+            if (flags != null)
+                setHdbDerivationFlag(output, flags);
+            //
+            /* added to allow users to automatically set the Validation column */
+            if (validation_flag.length() > 0)
+                setHdbValidationFlag(output, validation_flag.charAt(1));
 
-                info("Setting output for " + debugSdf.format(cal.getTime()));
-                setOutput(output, coeff, cal.getTime());
-            }
+            info("Setting output for " + debugSdf.format(cal.getTime()));
+            setOutput(output, ratio, cal.getTime());
         }
         // delete any existing value if this calculation failed
         else
         {
             info("Deleting output for " + debugSdf.format(cal.getTime()));
-            for (Object mon: mons)
-            {
-                cal.set(coeff_year,Integer.parseInt(mon.toString()) - 1,1,0,0);
-                deleteOutput(output,cal.getTime());
-            }
+            cal.set(coeff_year,0,1,0,0);
+            deleteOutput(output,cal.getTime());
         }
 
 //AW:AFTER_TIMESLICES_END
