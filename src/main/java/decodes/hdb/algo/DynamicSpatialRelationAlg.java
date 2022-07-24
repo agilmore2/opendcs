ede7b784fb17309d6d14705ad4f63684df425552
Andrew Gilmore <andrew@precisionwre.com>
2022-07-23 23:18:17 -0600

    Initial Spatial Relation Algorithm
    Signed-off-by: Andrew Gilmore <andrew@precisionwre.com>

---
+++ src/main/java/decodes/hdb/algo/DynamicSpatialRelationAlg.java
@@ -0,0 +1,406 @@
+package decodes.hdb.algo;
+
+import decodes.hdb.HdbFlags;
+import decodes.hdb.dbutils.DBAccess;
+import decodes.hdb.dbutils.DataObject;
+import decodes.tsdb.*;
+import decodes.tsdb.algo.AWAlgoType;
+import decodes.util.DecodesSettings;
+import decodes.util.PropertySpec;
+import ilex.util.TextUtil;
+import ilex.var.NamedVariable;
+import ilex.var.TimedVariable;
+import opendcs.dai.TimeSeriesDAI;
+
+import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static decodes.tsdb.VarFlags.TO_DELETE;
+import static decodes.tsdb.VarFlags.TO_WRITE;
+
+//AW:IMPORTS
+// Place an import statements you need here.
+//AW:IMPORTS_END
+
+//AW:JAVADOC
+
+/**
+ This algorithm spatially aggregates sites to a target site defined by
+ an attribute id and ref_spatial_relation. Does any aggregating function other than SUM make sense?
+
+ ATTRIBUTE: HDB_ATTR entry to use:
+ If attr_value_type is string or date, each site contributes 100% to site_b
+ If attr_value_type is number, the ref_spatial_relation VALUE field is used to weight contributions
+ ROUNDING: determines if rounding to the 5th decimal point is desired, default FALSE
+
+ Algorithm is as follows:
+ Sum all sites found using same attribute and site_b, using weights if necessary
+ Find output sdi as site_b for same datatype
+ Store as time-slice output on the determined site.
+
+ working SQL queries to compute data:
+
+ WITH t AS
+ (SELECT r.value value, sr_as.value weight, sr_as.attr_id, sr_as.b_site_id output_site
+ FROM r_month r, ref_spatial_relation sr_trig,
+ ref_spatial_relation sr_as,
+ hdb_site_datatype trig, hdb_site_datatype members
+ WHERE
+ trig.site_datatype_id = 7616 AND
+ sr_trig.a_site_id = trig.site_id AND
+ sr_trig.attr_id = 339 AND
+ sr_as.attr_id = sr_trig.attr_id AND
+ sr_as.b_site_id = sr_trig.b_site_id AND
+ members.site_id = sr_as.a_site_id AND
+ members.datatype_id = trig.datatype_id AND
+ r.site_datatype_id = members.site_datatype_id AND
+ r.start_date_time = '01-JAN-1990')
+ SELECT output_site,sum(t.VALUE*decode(a.attr_value_type,'number',t.weight,1.0)) value, sum(t.weight) total-- use weight if attr type is number, otherwise 1.0
+ FROM t, hdb_attr a
+ where a.attr_id = t.attr_id
+ group by output_site;
+
+ To find output sdis:
+
+ SELECT
+ outputsdis.site_datatype_id
+ FROM hdb_site_datatype sourcesd, ref_spatial_relation sr, hdb_site_datatype outputsdis
+ WHERE
+ sourcesd.site_datatype_id = 7630 AND
+ sr.attr_id = 339 AND
+ sr.a_site_id = sourcesd.site_id AND
+ outputsdis.site_id = sr.b_site_id AND
+ outputsdis.datatype_id = sourcesd.datatype_id
+ ;
+
+ */
+//AW:JAVADOC_END
+public class DynamicSpatialRelationAlg
+        extends decodes.tsdb.algo.AW_AlgorithmBase
+{
+    //AW:INPUTS
+    public double input;	//AW:TYPECODE=i
+    String[] _inputNames = { "input" };
+//AW:INPUTS_END
+
+    //AW:LOCALVARS
+    // Enter any local class variables needed by the algorithm.
+    String alg_ver = "1.0";
+    String query;
+    String selectClause;
+    Connection conn = null;
+    DBAccess db = null;
+    DataObject dbobj = null;
+    TimeSeriesDAI dao = null;
+    HashMap<String,CTimeSeries> outputSeries = null;
+
+    PropertySpec[] specs =
+            {
+                    new PropertySpec("rounding", PropertySpec.BOOLEAN,
+                            "(default=false) If true, then rounding is done on the output value"),
+                    new PropertySpec("validation_flag", PropertySpec.STRING,
+                            "(empty) Always set this validation flag in the output"),
+                    new PropertySpec("flags", PropertySpec.STRING,
+                            "(empty) Always set these dataflags in the output"),
+                    new PropertySpec("attribute", PropertySpec.STRING,
+                            "(empty) Which attribute should be aggregated"),
+            };
+
+
+
+//AW:LOCALVARS_END
+
+    //AW:OUTPUTS
+    String[] _outputNames = {};
+
+//AW:OUTPUTS_END
+
+    //AW:PROPERTIES
+    public boolean rounding = false;
+    public String validation_flag = "";
+    public String flags = "";
+    public Integer attribute = null;
+    public String peer_site_method = "HUC";
+
+    String[] _propertyNames = { "rounding", "validation_flag", "flags", "attribute"};
+
+//AW:PROPERTIES_END
+
+    // Allow javac to generate a no-args constructor.
+
+    /**
+     * Algorithm-specific initialization provided by the subclass.
+     */
+    protected void initAWAlgorithm( )
+            throws DbCompException
+    {
+//AW:INIT
+        _awAlgoType = AWAlgoType.TIME_SLICE;
+//AW:INIT_END
+
+//AW:USERINIT
+//AW:USERINIT_END
+    }
+
+    /**
+     * This method is called once before iterating all time slices.
+     */
+    protected void beforeTimeSlices()
+            throws DbCompException
+    {
+//AW:BEFORE_TIMESLICES
+        // This code will be executed once before each group of time slices.
+        // For TimeSlice algorithms this is done once before all slices.
+        // For Aggregating algorithms, this is done before each aggregate
+        // period.
+
+
+        dbobj.put("ALG_VERSION",alg_ver);
+        conn = tsdb.getConnection();
+        db = new DBAccess(conn);
+        dao = tsdb.makeTimeSeriesDAO();
+        String status;
+
+        //get output TS_IDs
+        query = " SELECT " +
+                " id.ts_id ts, outputsdis.site_id " +
+                " FROM hdb_site_datatype sourcesd, ref_spatial_relation sr, " +
+                " hdb_site_datatype outputsdis, cp_ts_id id " +
+                " WHERE " +
+                " sourcesd.site_datatype_id = " + getSDI("input") + " AND " +
+                " sr.attr_id = " + attribute + " AND " +
+                " sr.a_site_id = sourcesd.site_id AND " +
+                " outputsdis.site_id = sr.b_site_id AND " +
+                " outputsdis.datatype_id = sourcesd.datatype_id AND " +
+                " id.site_datatype_id = outputsdis.site_datatype_id AND " +
+                " id.interval = '" + getInterval("input") + "' AND " +
+                " id.table_selector = " + getTableSelector("input");
+
+        debug3("Before TimeSlice Query: " + query);
+
+        status = db.performQuery(query,dbobj);
+        if (status.startsWith("ERROR"))
+        {
+            warning(comp.getName() + "-" + alg_ver + " Aborted: see following error message");
+            warning(status);
+            return;
+        }
+        // Need to handle multiple TS_IDs!
+        int count = Integer.parseInt(dbobj.get("rowCount").toString());
+        Object t = dbobj.get("ts");
+        ArrayList<Object> tsids;
+
+        if (count == 0)
+        {
+            warning(comp.getName() + "-" + alg_ver + " Aborted: zero output TS_IDs");
+            warning(status);
+            return;
+        }
+        else if (count == 1)
+        {
+            tsids = new ArrayList<Object>();
+            tsids.add(t);
+        }
+        else
+        {
+            tsids = (ArrayList<Object>)t;
+        }
+
+        Iterator<Object> itId = tsids.iterator();
+        try {
+            while(itId.hasNext()) {
+                String id = itId.next().toString();
+                debug3("Output Timeseries ID: " + id);
+                TimeSeriesIdentifier tsid = dao.getTimeSeriesIdentifier(id);
+                outputSeries.put(id,dao.makeTimeSeries(tsid));
+            }
+        } catch (Exception e) {
+            warning(e.toString());
+            return;
+        }
+
+        // now build query for sums
+        query = "  WITH t AS " +
+                " (SELECT r.value value, sr_as.value weight, sr_as.attr_id, sr_as.b_site_id output_site " +
+                " FROM r_month r, ref_spatial_relation sr_trig, " +
+                " ref_spatial_relation sr_as, " +
+                " hdb_site_datatype trig, hdb_site_datatype members " +
+                " WHERE " +
+                " trig.site_datatype_id = " + getSDI("input") + " AND " +
+                " sr_trig.a_site_id = trig.site_id AND " +
+                " sr_trig.attr_id = " + attribute + " AND " +
+                " sr_as.attr_id = sr_trig.attr_id AND " +
+                " sr_as.b_site_id = sr_trig.b_site_id AND " +
+                " members.site_id = sr_as.a_site_id AND " +
+                " members.datatype_id = trig.datatype_id AND " +
+                " r.site_datatype_id = members.site_datatype_id AND ";
+
+        selectClause = " SELECT output_site, ";
+        if (rounding)
+        {
+            selectClause += " round(sum(t.VALUE*decode(a.attr_value_type,'number', t.weight,1.0)),7) value"; // 7 used by other HDB aggregates
+        } else
+        {
+            selectClause += " sum(t.VALUE*decode(a.attr_value_type,'number', t.weight,1.0)) value, ";
+        }
+        selectClause += " sum(t.weight) total-- use weight if attr type is number, otherwise 1.0 " +
+                        " FROM t, hdb_attr a " +
+                        " where a.attr_id = t.attr_id " +
+                        " group by output_site";
+
+//AW:BEFORE_TIMESLICES_END
+    }
+
+    /**
+     * Do the algorithm for a single time slice.
+     * AW will fill in user-supplied code here.
+     * Base class will set inputs prior to calling this method.
+     * User code should call one of the setOutput methods for a time-slice
+     * output variable.
+     *
+     * @throws DbCompException (or subclass thereof) if execution of this
+     *        algorithm is to be aborted.
+     */
+    protected void doAWTimeSlice()
+            throws DbCompException
+    {
+//AW:TIMESLICE
+        debug3(comp.getName() + "-" + alg_ver + " BEGINNING OF doAWTimeSlice for period: " +
+                _timeSliceBaseTime + " SDI: " + getSDI("input"));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        sdf.setTimeZone(TimeZone.getTimeZone(DecodesSettings.instance().aggregateTimeZone));
+        String status;
+
+        String dateQuery = " r.start_date_time = to_date('" + sdf.format(_timeSliceBaseTime) + "'," +
+                           "'dd-MM-yyyy HH24:MI'))\n"; // last ) to close WITH statement
+
+        String sliceQuery = query + dateQuery + selectClause;
+        debug3("TimeSlice Query: " + sliceQuery);
+
+        status = db.performQuery(sliceQuery,dbobj); // query returns output site id, value, and total weight
+        int count = Integer.parseInt(dbobj.get("rowCount").toString());
+        if (status.startsWith("ERROR") || count != outputSeries.size())
+        {
+            warning(comp.getName() + "-" + alg_ver + " TimeSlice aborted at: " + debugSdf.format(this._timeSliceBaseTime) +
+                    " See following error message:");
+            warning(status);
+            // deletions?!?
+            //outputSeries.addSample(new TimedVariable(_timeSliceBaseTime, 0, TO_DELETE));
+        }
+        else
+        {
+            ArrayList<Object> outputs;
+            ArrayList<Object> values;
+            ArrayList<Object> weights;
+            Object o = dbobj.get("output");
+            Object v = dbobj.get("value");
+            Object w = dbobj.get("weight");
+            if (count == 1)
+            {
+                outputs = new ArrayList<Object>();
+                values = new ArrayList<Object>();
+                weights = new ArrayList<Object>();
+                outputs.add(o);
+                values.add(v);
+                weights.add(w);
+            }
+            else
+            {
+                outputs = (ArrayList<Object>)o;
+                values = (ArrayList<Object>)v;
+                weights = (ArrayList<Object>)w;
+            }
+
+            Iterator<Object> itO = outputs.iterator();
+            Iterator<Object> itV = values.iterator();
+            Iterator<Object> itW = weights.iterator();
+            try {
+                while(itO.hasNext() && itV.hasNext() && itW.hasNext()) {
+                    String id = itO.next().toString();
+                    debug3("Output Site ID: " + id);
+                    Double value_out = new Double(itV.next().toString());
+                    Double weight_out = new Double(itW.next().toString());
+                    debug3(comp.getName() + "-" + alg_ver + "Setting output for timeslice: " + debugSdf.format(this._timeSliceBaseTime) +
+                            " value: " + value_out + " weight: " + weight_out);
+                    TimedVariable tv = new TimedVariable(_timeSliceBaseTime, value_out, TO_WRITE);
+                    if (flags.length() > 0)
+                        tv.setFlags(tv.getFlags() | HdbFlags.hdbDerivation2flag(flags));
+                    if (validation_flag.length() > 0)
+                        tv.setFlags(tv.getFlags() | HdbFlags.hdbValidation2flag(validation_flag.charAt(1)));
+                    if (tsdb.isHdb() && TextUtil.str2boolean(comp.getProperty("OverwriteFlag")))
+                    {
+                        //waiting on release of overwrite flag upstream
+                        //v.setFlags(v.getFlags() | HdbFlags.HDBF_OVERWRITE_FLAG);
+                    }
+                    outputSeries.get(id).addSample(tv);
+                }
+            } catch (Exception e) {
+                warning(e.toString());
+                return;
+            }
+        }
+
+        // Enter code to be executed at each time-slice.
+//AW:TIMESLICE_END
+    }
+
+    /**
+     * This method is called once after iterating all time slices.
+     */
+    protected void afterTimeSlices()
+            throws DbCompException
+    {
+//AW:AFTER_TIMESLICES
+        // This code will be executed once after each group of time slices.
+        // For TimeSlice algorithms this is done once after all slices.
+        // For Aggregating algorithms, this is done after each aggregate
+        // period.
+
+        // set the outputs. If some timesteps failed, must be marked for delete above
+        for (Map.Entry<String, CTimeSeries> entry : outputSeries.entrySet()) {
+            String k = entry.getKey();
+            CTimeSeries v = entry.getValue();
+            try {
+                debug1(comp.getName() + "-" + alg_ver + "saving site: " + k + " timeseries: " + v.getBriefDescription());
+                dao.saveTimeSeries(v);
+            } catch (Exception e) {
+                warning("Exception during saving output to database:" + e);
+            }
+        }
+//AW:AFTER_TIMESLICES_END
+    }
+
+    /**
+     * Required method returns a list of all input time series names.
+     */
+    public String[] getInputNames()
+    {
+        return _inputNames;
+    }
+
+    /**
+     * Required method returns a list of all output time series names.
+     */
+    public String[] getOutputNames()
+    {
+        return _outputNames;
+    }
+
+    /**
+     * Required method returns a list of properties that have meaning to
+     * this algorithm.
+     */
+    public String[] getPropertyNames()
+    {
+        return _propertyNames;
+    }
+
+    @Override
+    protected PropertySpec[] getAlgoPropertySpecs()
+    {
+        return specs;
+    }
+
+}
