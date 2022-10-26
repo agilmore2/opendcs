af2e596bda8e211b966e210aef9e66cc57795a42
Andrew Gilmore <andrew@precisionwre.com>
2022-10-26 12:24:33 -0600

    Remove fill end year and make it fill to current month. Cleanups and use of dates_between function
    Signed-off-by: Andrew Gilmore <andrew@precisionwre.com>

--- src/main/java/decodes/hdb/algo/CULFillMissingClimateData.java
+++ src/main/java/decodes/hdb/algo/CULFillMissingClimateData.java
@@ -1,9 +1,7 @@
 package decodes.hdb.algo;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -11,21 +9,14 @@ import java.sql.Connection;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
-import ilex.var.NamedVariableList;
 import ilex.var.NamedVariable;
 import decodes.hdb.dbutils.DBAccess;
 import decodes.hdb.dbutils.DataObject;
 import decodes.sql.DbKey;
-import decodes.tsdb.DbAlgorithmExecutive;
 import decodes.tsdb.DbCompException;
-import decodes.tsdb.DbIoException;
-import decodes.tsdb.VarFlags;
 import decodes.tsdb.algo.AWAlgoType;
-import decodes.tsdb.CTimeSeries;
 import decodes.tsdb.ParmRef;
 import decodes.util.PropertySpec;
-import ilex.var.TimedVariable;
-import decodes.tsdb.TimeSeriesIdentifier;
 
 //AW:IMPORTS
 // Place an import statements you need here.
@@ -35,6 +26,8 @@ import decodes.tsdb.TimeSeriesIdentifier;
 /**
 This algorithm fills missing climate data for the CUL climate stations used to estimate past reservoir evaporation.
 When a site is missing precip or temperature data, use the average of all other data points at that site for the current month.
+
+ Specific to r_month data
 
 The following queries were used to develop this algorithm:
 Compute monthly averages:
@@ -91,8 +84,6 @@ public class CULFillMissingClimateData
 		{
 				new PropertySpec("fillStartYr", PropertySpec.INT,
 					"(1971) First year to fill data"),
-				new PropertySpec("fillEndYr", PropertySpec.INT,
-					"(2020) Last year to fill data"),
 				new PropertySpec("estimation_process", PropertySpec.STRING,
                         "(CU_Agg_Disagg) Which loading application produces estimates that should be ignored."),
 
@@ -101,14 +92,13 @@ public class CULFillMissingClimateData
 
 //AW:OUTPUTS
 	public NamedVariable output = new NamedVariable("output",0);
-	String _outputNames[] = { "output" };
+	String[] _outputNames = { "output" };
 //AW:OUTPUTS_END
 
 //AW:PROPERTIES
 	public long fillStartYr = 1971;
-	public long fillEndYr = 2020;
 	public String estimation_process = "CU_Agg_Disagg";
-	String _propertyNames[] = { "fillStartYr","fillEndYr","estimation_process" };
+	String[] _propertyNames = { "fillStartYr", "estimation_process" };
 //AW:PROPERTIES_END
 
 	// Allow javac to generate a no-args constructor.
@@ -174,13 +164,8 @@ public class CULFillMissingClimateData
 		// For Aggregating algorithms, this is done after each aggregate
 		// period.
 		
-		// TODO: is this section necessary?
-        TimeZone tz = TimeZone.getTimeZone("MST");
-        GregorianCalendar cal = new GregorianCalendar(tz);
-        GregorianCalendar cal1 = new GregorianCalendar(); //uses correct timezone from OpenDCS properties
-        cal1.setTime(_timeSliceBaseTime);
-        cal.set(cal1.get(Calendar.YEAR),cal1.get(Calendar.MONTH),cal1.get(Calendar.DAY_OF_MONTH),0,0);
-        
+        TimeZone tz = TimeZone.getTimeZone(aggregateTimeZone);
+
         conn = tsdb.getConnection();
         String status;
         DataObject dbobj = new DataObject();
@@ -188,19 +173,21 @@ public class CULFillMissingClimateData
         DBAccess db = new DBAccess(conn);
         
         // Query to get monthly averages from source data
-        query = "SELECT EXTRACT(MONTH FROM start_date_time) month, AVG(value) avg FROM r_base\n" 
-    			+ "WHERE interval = 'month'\n"
-        		+ "AND site_datatype_id = " + sdi + "\n"
-        		+ "AND loading_application_id NOT IN\n"
-        		+	"(\n"
-        		+	"SELECT loading_application_id FROM hdb_loading_application WHERE\n"
-        		+	"loading_application_name IN('" + estimation_process + "','CU_FillMissing')\n"
-        		+	")\n"
-        		+ "GROUP BY EXTRACT(MONTH FROM start_date_time)\n" 
-        		+ "ORDER BY month\n";
+        query = "SELECT EXTRACT(MONTH FROM r.start_date_time) month, AVG(r.value) avg FROM r_month r, r_base b "
+    			+ "WHERE b.interval = 'month' "
+				+ "AND r.site_datatype_id = " + sdi + " "
+				+ "AND r.site_datatype_id = b.site_datatype_id "
+        		+ "AND r.start_date_time = b.start_date_time "
+        		+ "AND b.loading_application_id NOT IN "
+        		+	"( "
+        		+	"SELECT loading_application_id FROM hdb_loading_application WHERE "
+        		+	"loading_application_name IN('" + estimation_process + "','CU_FillMissing') "
+        		+	") "
+        		+ "GROUP BY EXTRACT(MONTH FROM r.start_date_time) "
+        		+ "ORDER BY month ";
         
         status = db.performQuery(query,dbobj);
-        debug3(" SQL STRING:" + query + "   DBOBJ: " + dbobj.toString() + "STATUS:  " + status);
+        debug3(" SQL STRING:" + query + "   DBOBJ: " + dbobj + "STATUS:  " + status);
         
         if (status.startsWith("ERROR") || ((Integer) dbobj.get("rowCount")) != 12)
         {
@@ -213,28 +200,25 @@ public class CULFillMissingClimateData
         ArrayList<Object> monthlyAvgs = (ArrayList<Object>) dbobj.get("avg");
         
         // Query to get months to fill
-        query = "WITH allDates AS\n"
-        		+ "(\n"
-        		+ "SELECT ADD_MONTHS(DATE '" + fillStartYr + "-01-01',level - 1) dt\n"
-        		+ "FROM dual\n"
-        		+ "CONNECT BY LEVEL <= \n"
-        		+ "(SELECT MONTHS_BETWEEN(DATE '" + fillEndYr + "-12-01',DATE '" + fillStartYr + "-01-01') + 1 FROM dual)\n"
-        		+ ")\n"
-        		+ "SELECT dt FROM allDates WHERE dt NOT IN\n"
-        		+ "(\n"
-        		+ "SELECT start_date_time FROM r_base\n"
-        		+ "WHERE interval = 'month'\n"
-        		+ "AND site_datatype_id = " + sdi + "\n"
-        		+ "AND loading_application_id NOT IN\n"
-        		+ "	(\n"
-        		+ "	SELECT loading_application_id FROM hdb_loading_application WHERE\n"
-        		+ "	loading_application_name IN('" + estimation_process + "','CU_FillMissing')\n"
-        		+ "	)\n"
-        		+ ")\n"
+        query = "WITH allDates AS "
+        		+ "( "
+        		+ "select date_time dt from table(dates_between(DATE '" + fillStartYr + "-01-01',trunc(sysdate, 'month'), 'month')) "
+        		+ ") "
+        		+ "SELECT dt FROM allDates WHERE dt NOT IN "
+        		+ "( "
+        		+ "SELECT start_date_time FROM r_base "
+        		+ "WHERE interval = 'month' "
+        		+ "AND site_datatype_id = " + sdi + " "
+        		+ "AND loading_application_id NOT IN "
+        		+ "	( "
+        		+ "	SELECT loading_application_id FROM hdb_loading_application WHERE "
+        		+ "	loading_application_name IN('" + estimation_process + "','CU_FillMissing') "
+        		+ "	) "
+        		+ ") "
         		+ "ORDER BY dt";
         
         status = db.performQuery(query,dbobj);
-        debug3(" SQL STRING:" + query + "   DBOBJ: " + dbobj.toString() + "STATUS:  " + status);
+        debug3(" SQL STRING:" + query + "   DBOBJ: " + dbobj + "STATUS:  " + status);
         
         if (status.startsWith("ERROR") || ((Integer) dbobj.get("rowCount")) == 0)
         {
@@ -247,19 +231,19 @@ public class CULFillMissingClimateData
         ArrayList<Object> monthsToFill;
         if (((Integer) dbobj.get("rowCount")) == 1)
         {
-        	monthsToFill = new ArrayList<Object>(1);
+        	monthsToFill = new ArrayList<>(1);
         	monthsToFill.add(dbobj.get("dt"));
-        }else
+        } else
         {
         	monthsToFill = (ArrayList<Object>) dbobj.get("dt");
         }
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.S",Locale.ENGLISH);
         formatter.setTimeZone(tz);
-        for(Iterator iter = monthsToFill.iterator(); iter.hasNext();)
+        for(Iterator<Object> iter = monthsToFill.iterator(); iter.hasNext();)
         {
         	try {
 				Date dt = formatter.parse((String) iter.next());
-				Double avg = Double.parseDouble((String) monthlyAvgs.get(dt.getMonth())); // seems more complicated than it should need to be, but sometimes SQL was returning results in scientific notation
+				double avg = Double.parseDouble((String) monthlyAvgs.get(dt.getMonth())); // seems more complicated than it should need to be, but sometimes SQL was returning results in scientific notation
 				setOutput(output,avg,dt);
 			} catch (ParseException e) {
 				// improve error handling
